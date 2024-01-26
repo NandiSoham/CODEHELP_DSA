d95fce0a579a68b08f809168f7907fe6b9b8b400
SohamNandiPDX <snandi@pdx.edu>
2024-01-26 00:58:55 -0800

    optimized the code for finding second largest and smallest element

--- 02_arrays/02_findSecondLargestAndSecondSmallest.cpp
+++ 02_arrays/02_findSecondLargestAndSecondSmallest.cpp
@@ -2,36 +2,81 @@
 #include<limits.h>
 using namespace std;
 
-void getElements(int arr[], int n){
-    int smallest = INT_MAX, secSmall = INT_MAX;
-    int largest = INT_MIN, secLarge = INT_MIN;
+//-------------------------------------better approach----------------------------------------------------------------------
+// void getElements(int arr[], int n){
+//     int smallest = INT_MAX, secSmall = INT_MAX;
+//     int largest = INT_MIN, secLarge = INT_MIN;
 
-    for(int i= 0; i < n; i++){
-        smallest = min(smallest, arr[i]);
-        largest = max(largest, arr[i]);
+//     for(int i= 0; i < n; i++){
+//         smallest = min(smallest, arr[i]);
+//         largest = max(largest, arr[i]);
+//     }
+
+//     for(int i = 0; i < n; i++){
+//         if(n==0 || n==1){
+//             cout << -1 << " " << -1 << endl;
+//         }
+
+//         if(arr[i] < secSmall && arr[i] != smallest){
+//             secSmall = arr[i];
+//         }
+//         if(arr[i] > secLarge && arr[i] != largest){
+//             secLarge = arr[i];
+//         }
+//     }
+
+//     cout << "Second smallest element is: " << secSmall << endl;
+//     cout << "Second largest element is: " << secLarge << endl;
+// }
+
+// int main()
+// {
+//     int arr[] = {1, 2, 4, 6, 7, 5};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     getElements(arr, n);
+//     return 0;
+// }
+
+//---------------------------------------------------------------------------------
+
+//-------------------------------------optimal approach----------------------------------------------------------------------
+
+int findSecondLargest(int *arr, int n){
+    if(n < 2) return -1;
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if(arr[i] >  largest){
+            secondLargest = largest;
+            largest = arr[i];
+        } else if(arr[i] > secondLargest && arr[i] != largest){
+            secondLargest = arr[i];
+        }
     }
-
-    for(int i = 0; i < n; i++){
-        if(n==0 || n==1){
-            cout << -1 << " " << -1 << endl;
-        }
-
-        if(arr[i] < secSmall && arr[i] != smallest){
-            secSmall = arr[i];
-        }
-        if(arr[i] > secLarge && arr[i] != largest){
-            secLarge = arr[i];
-        }
-    }
-
-    cout << "Second smallest element is: " << secSmall << endl;
-    cout << "Second largest element is: " << secLarge << endl;
+    return secondLargest;
 }
 
-int main()
-{
-    int arr[] = {1, 2, 4, 6, 7, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    getElements(arr, n);
+int findSecondSmallest(int *arr, int n){
+    if(n < 2) return -1;
+    int smallest = INT_MAX;
+    int secondSmallest = INT_MAX;
+    for(int i = 0; i < n; i++){
+        if (arr[i] < smallest){
+            secondSmallest = smallest;
+            smallest = arr[i];
+        } else if(arr[i] < secondSmallest && arr[i] != smallest){
+            secondSmallest = arr[i];
+        }
+    }
+    return secondSmallest;
+}
+
+int main() {
+    int arr[]={1,2,4,7,7,5};  
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int secondSmallest= findSecondSmallest(arr,n);
+    int secondLargest= findSecondLargest(arr,n);
+    cout<<"Second smallest is "<<secondSmallest<<endl;
+    cout<<"Second largest is "<<secondLargest<<endl;
     return 0;
 }
