ba67fda0567f1eedf64d408f6737873e693c5fb2
SohamNandiPDX <snandi@pdx.edu>
2024-01-28 22:38:30 -0800

    Move all Zeros to the end of the array solved WITH OPTIMAL APPROACH

--- 02_arrays/06_moveZerosToEnd.cpp
+++ 02_arrays/06_moveZerosToEnd.cpp
@@ -1,23 +1,53 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
+//----------------------------BRUTE FORCE APPROACH-----------------------
+// void moveZeros(std::vector<int>& arr, int n){
+//     vector<int> temp;
+//     for(int i = 0; i < n; i++){
+//         if(arr[i] != 0){
+//             temp.push_back(arr[i]);
+//         }
+//     }
+
+//     for(int i = 0; i < temp.size(); i++){
+//         arr[i] = temp[i];
+//     }
+
+//     for(int i = temp.size(); i < arr.size(); i++){
+//         arr[i] = 0;
+//     }
+// }
+//-----------------------------------------------------------------------------------
+
+//--------------------OPTIMAL APPROACH - TWO POINTERS ----------------------------------------
 void moveZeros(std::vector<int>& arr, int n){
-    vector<int> temp;
-    for(int i = 0; i < n; i++){
-        if(arr[i] != 0){
-            temp.push_back(arr[i]);
+        int j = -1;
+
+        // Find the index of the first zero
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                j = i;
+                break;
+            }
         }
-    }
 
-    for(int i = 0; i < temp.size(); i++){
-        arr[i] = temp[i];
-    }
+        // If no zero is found, return
+        if (j == -1) {
+            return;
+        }
 
-    for(int i = temp.size(); i < arr.size(); i++){
-        arr[i] = 0;
-    }
+        // Move non-zero elements to the left
+        for (int i = j + 1; i < n; i++) {
+            if (arr[i] != 0) {
+                swap(arr[j], arr[i]);
+                j++;
+            }
+        }
 }
+//-----------------------------------------------------------------------------------
 
 int main()
 {
