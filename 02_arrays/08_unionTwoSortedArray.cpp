70ac3bdbad248d59d571531b3f9e3462f8312f3f
SohamNandiPDX <snandi@pdx.edu>
2024-01-29 23:42:03 -0800

    Union of Two Sorted Arrays solved in optimal way

--- 02_arrays/08_unionTwoSortedArray.cpp
+++ 02_arrays/08_unionTwoSortedArray.cpp
@@ -4,6 +4,8 @@
 #include<algorithm>
 using namespace std;
 
+//-------------------------BRUTE FORCE - using SET-----------------------------------------
+
 vector < int > unionArray(vector < int >& a, vector < int >& b) {
 
     set < int > st;
@@ -24,6 +26,51 @@ vector < int > unionArray(vector < int >& a, vector < int >& b) {
     return ans;
 }
 
+//---------------------------------------------------------------------------------------------------------
+
+//------------------------------ Optimal - using two pointers------------------------------------------------------
+
+vector < int > unionArrayUsingTwoPointers(vector < int > a, vector < int > b) {
+    // Write your code here
+    int n1 = a.size();
+    int n2 = b.size();
+    int  i = 0, j = 0;
+    vector <int> ans;
+    
+    while(i < n1 && j < n2){
+        if(a[i] <= b[j]){
+            if(ans.size() == 0 || ans.back() != a[i]){
+                ans.push_back(a[i]);
+            }
+            i++;
+        }
+        else{
+            if(ans.size() == 0 || ans.back() != b[j]){
+                ans.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while(i < n1){
+            if(ans.size() == 0 || ans.back() != a[i]){
+                ans.push_back(a[i]);
+            }
+            i++;
+    }
+
+    while(j < n2){
+        if(ans.size() == 0 || ans.back() != b[j]){
+                ans.push_back(b[j]);
+            }
+            j++;
+    }
+
+    return ans;
+}
+
+//---------------------------------------------------------------------------------------------------------
+
 int main()
 {
     vector < int > a{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -31,6 +78,7 @@ int main()
     int n1 = a.size();
     int n2 = b.size();
     vector <int> ans = unionArray(a, b);
+    vector <int> ans = unionArrayUsingTwoPointers(a, b);
     cout << "Union of the arrays is : " << endl;
     for(auto & it:ans){
         cout << it << " ";
