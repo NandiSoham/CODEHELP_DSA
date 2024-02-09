1d181bb5e36aa9c79a44cf85a061eb6d7bd7dcb0
SohamNandiPDX <snandi@pdx.edu>
2024-02-08 23:13:28 -0800

    solved supreme elements in optimal approach

--- 02_arrays/19_superiorElements.cpp
+++ 02_arrays/19_superiorElements.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
+//-----------------------------------------BRUTE FORCE---------------------------
 vector<int> printLeadersBruteForce(int arr[], int n) {
 
   vector<int> ans;
@@ -9,18 +9,11 @@ vector<int> printLeadersBruteForce(int arr[], int n) {
   for (int i = 0; i < n; i++) {
     bool leader = true;
 
-    //Checking whether arr[i] is greater than all 
-    //the elements in its right side
     for (int j = i + 1; j < n; j++)
       if (arr[j] > arr[i]) {
-          
-        // If any element found is greater than current leader
-        // curr element is not the leader.
         leader = false;
         break;
       }
-
-    // Push all the leaders in ans array.
     if (leader)
     ans.push_back(arr[i]);
 
@@ -28,21 +21,37 @@ vector<int> printLeadersBruteForce(int arr[], int n) {
   
   return ans;
 }
+//-------------------------------------------------------------------------------------
+
+//---------------------------------Optimal-----------------------------------------------
+vector<int> superiorElements(int arr[], int n) {
+
+    vector<int> ans;
+    int max = INT_MIN;
+
+    for (int i = n - 1; i >= 0; i--)
+        if (arr[i] > max) {
+        ans.push_back(arr[i]);
+        max = arr[i];
+        }
+
+    
+    return ans;
+}
+//------------------------------------------------------------------------------------------
 
 int main() {
-    
-  // Array Initialization.
-  int n = 6;
-  int arr[n] = {10, 22, 12, 3, 0, 6};
+    int n = 6;
+    int arr[n] = {10, 22, 12, 3, 0, 6};
 
-  vector<int> ans = printLeadersBruteForce(arr,n);
-  
-  for(int i = 0;i<ans.size();i++){
+    // vector<int> ans = printLeadersBruteForce(arr,n);
+    vector<int> ans = superiorElements(arr,n);
+    for(int i = 0;i<ans.size();i++){
       
-      cout<<ans[i]<<" ";
-  }
+        cout<<ans[i]<<" ";
+    }
   
-  cout<<endl;
-  return 0;
+    cout<<endl;
+    return 0;
 }
 
