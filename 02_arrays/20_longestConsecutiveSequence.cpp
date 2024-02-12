f7345e79cba5d72643944fbf65351946e58aa7d0
SohamNandiPDX <snandi@pdx.edu>
2024-02-12 01:47:42 -0800

    solved longest consecutive sequence using optimal approach

--- 02_arrays/20_longestConsecutiveSequence.cpp
+++ 02_arrays/20_longestConsecutiveSequence.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include<algorithm>
+#include <unordered_set>
 using namespace std;
 
 //---------------------------------BRUTE FORCE------------------------------
@@ -57,12 +58,42 @@ int longestConsecutiveSequence(vector<int> &arr){
 
 //----------------------------------------------------------------
 
+//--------------------------OPTIMAL APPROACH--------------------------------------
+
+int longestConsecutiveSequenceOptimal(vector<int> &arr){
+    int longest = 1;
+    unordered_set<int> st;
+
+    if(arr.size() == 0) return 0;
+
+    for(int i = 0; i < arr.size(); i++){
+        st.insert(arr[i]);
+    }
+    for(auto it : st){
+        if(st.find(it - 1) == st.end()){
+            int cnt = 1;
+            int x = it;
+            while(st.find(x+1) != st.end()) {
+                x += 1;
+                cnt += 1;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+    return longest;
+}
+
+//------------------------------------------------------------------------------
+
 int main()
 {
     vector<int> arr = {100, 200, 1, 2, 3, 4};
     int ans = longestConsecutive(arr);
     int ans2 = longestConsecutiveSequence(arr);
+    int ans3 = longestConsecutiveSequenceOptimal(arr);
+
     cout << "The length of the longest consecutive elements is: " << ans << endl;
     cout << "Using better  approach, The length of the longest consecutive elements is: " << ans2 << endl;
+    cout << "Using optimal approach, The length of the longest consecutive elements is: " << ans3 << endl;
     return 0;
 }
--- 02_arrays/tempCodeRunnerFile.cpp
+++ 02_arrays/tempCodeRunnerFile.cpp
@@ -1,11 +1,22 @@
-int findMaxSubarraySum(int arr[], int n){
-    int maxi = INT_MIN;
-    for(int i = 0; i < n; i++){
-        int sum = 0;
-        for(int j = i; j < n; j++){
-            sum += arr[j];
-            maxi = max(maxi, sum);
+int longestConsecutiveSequenceOptimal(vector<int> &arr){
+    int longest = 1;
+    unordered_set<int> st;
+
+    if(arr.size() == 0) return 0;
+
+    for(int i = 0; i < arr.size(); i++){
+        st.insert(arr[i]);
+    }
+    for(auto it : st){
+        if(st.find(it - 1) == st.end()){
+            int cnt = 1;
+            int x = it;
+            while(st.find(x+1) != st.end()) {
+                x += 1;
+                cnt += 1;
+            }
+            longest = max(longest, cnt);
         }
     }
-    return maxi;
+    return longest;
 }
