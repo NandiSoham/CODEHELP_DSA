be4a7e1ebeef4cb3871ba1f2f15cda7b8c1d4d0e
SohamNandiPDX <snandi@pdx.edu>
2024-02-13 22:34:58 -0800

    solved rotate matrix in optimal way

--- 02_arrays/22_rotateMatrixOrImage.cpp
+++ 02_arrays/22_rotateMatrixOrImage.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 using namespace std;
 
+//------------------------------ BRUTE FORCE---------------------------------------------
 vector < vector < int >> rotate(vector < vector < int >> & matrix) {
     int n = matrix.size();
     vector < vector < int >> rotated(n, vector < int > (n, 0));
@@ -13,14 +15,37 @@ vector < vector < int >> rotate(vector < vector < int >> & matrix) {
     return rotated;
 }
 
+//------------------------------------------------------------------------
+
+//------------------------------BETTER APPROACH-----------------------------
+
+vector < vector < int >> rotateOptimal(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < m; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+
+    return matrix;
+}
+
+
 int main() {
     vector < vector < int >> arr;
     arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    vector < vector < int >> rotated = rotate(arr);
+    // vector < vector < int >> rotated = rotate(arr);
+    vector < vector < int >> rotatedOptimal = rotateOptimal(arr);
     cout << "Rotated Image" << endl;
-    for (int i = 0; i < rotated.size(); i++) {
-    for (int j = 0; j < rotated[0].size(); j++) {
-        cout << rotated[i][j] << " ";
+    for (int i = 0; i < rotatedOptimal.size(); i++) {
+    for (int j = 0; j < rotatedOptimal[0].size(); j++) {
+        cout << rotatedOptimal[i][j] << " ";
     }
     cout << "\n";
     }
--- 02_arrays/tempCodeRunnerFile.cpp
+++ 02_arrays/tempCodeRunnerFile.cpp
@@ -1,35 +1,16 @@
-void markRow(vector<vector<int>> &matrix, int n, int m, int i){
-    for(int j = 0; j < n; j++){
-        if(matrix[i][j] != 0){
-            matrix[i][j] = -1;
-        }
-    }
-}
+vector < vector < int >> rotateOptimal(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
 
-void markCol(vector<vector<int>> &matrix, int n, int m, int j){
-    for(int i = 0; i < n; i++){
-        if(matrix[i][j] != 0){
-            matrix[i][j] = -1;
-        }
-    }
-}
-
-vector<vector<int>> zeroMatrix(vector<vector<int>>& matrix, int n, int m) {
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(matrix[i][j] == 0){
-                markRow(matrix, n, m, i);
-                markCol(matrix, n, m, j);
-            }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            swap(matrix[i][j], matrix[j][i]);
         }
     }
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(matrix[i][j] == -1){
-                matrix[i][j] = 0;
-            }
-        }
+        reverse(matrix.begin(), matrix.end());
     }
+
     return matrix;
 }
