fbee568f265ea44e5a9e2506049f2ca24041b198
SohamNandiPDX <snandi@pdx.edu>
2024-02-20 01:46:38 -0800

    solved majority elements-II in optimal approach

--- 02_arrays/26_majorityElement_II.cpp
+++ 02_arrays/26_majorityElement_II.cpp
@@ -49,13 +49,47 @@ vector<int> majorityElementBetterApproach(vector<int>& nums) {
 
 //----------------------------------------------------------------
 
+//------------------------OPTIMAL APPROACH------------------------
+vector<int> majorityElementOptimalApproach(vector<int>& nums) {
+        int n = nums.size();
+        int counter1 = 0, counter2 = 0;
+        int element1 = INT_MIN, element2 = INT_MIN;
+
+        for(int i = 0; i < n; i++){
+            if(counter1 == 0 && nums[i] != element2){
+                counter1 = 1;
+                element1 = nums[i];
+            } else if(counter2 == 0 && nums[i] != element1){
+                counter2 = 1;
+                element2 = nums[i];
+            } else if(nums[i] == element1) counter1++;
+            else if(nums[i] == element2) counter2++;
+            else counter1--, counter2--;
+        }
+
+        //manually check
+        int appearedCount1 = 0, appearedCount2 = 0;
+        vector<int> ans;
+        int mini = (n/3) + 1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == element1) appearedCount1++;
+            if(nums[i] == element2) appearedCount2++;
+        }
+        if(appearedCount1 >= mini) ans.push_back(element1);
+        if(appearedCount2 >= mini) ans.push_back(element2);
+        return ans;
+    }
+
+//----------------------------------------------------------------
+
 int main()
 {
     vector<int> arr {11, 22, 11, 22, 11, 22, 33, 33};
     // vector<int> ans = majorityElement(arr);
-    vector<int> ans2 = majorityElementBetterApproach(arr);
+    // vector<int> ans2 = majorityElementBetterApproach(arr);
+    vector<int> ans3 = majorityElementOptimalApproach(arr);
     cout << "The majority elements are: " << endl;
-    for(auto it : ans2){
+    for(auto it : ans3){
         cout << it << " ";
     }
     cout << endl;
