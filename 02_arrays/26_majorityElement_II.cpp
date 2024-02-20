83f57efbe2598053df3dd11d99c376fbeec63dea
SohamNandiPDX <snandi@pdx.edu>
2024-02-19 23:36:06 -0800

    solved majority elements-II in better approach

--- 02_arrays/26_majorityElement_II.cpp
+++ 02_arrays/26_majorityElement_II.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
+#include<unordered_map>
 #include<vector>
 using namespace std;
+
+//------------------------ BRUTE FORCE-------------------------------
 
 vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
@@ -23,13 +26,36 @@ vector<int> majorityElement(vector<int>& nums) {
         }
         return ans;
     }
+//----------------------------------------------------------------
+
+//---------------------------BETTER APPROACH----------------------
+
+vector<int> majorityElementBetterApproach(vector<int>& nums) {
+    int n = nums.size();
+        int mini = (n/3) + 1;
+        unordered_map<int, int> mpp;
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++){
+            mpp[nums[i]]++;
+
+            if(mpp[nums[i]] == mini){
+                ans.push_back(nums[i]);
+            }
+            if(ans.size() > 2) break;
+        }
+        return ans;
+}
+
+//----------------------------------------------------------------
 
 int main()
 {
     vector<int> arr {11, 22, 11, 22, 11, 22, 33, 33};
-    vector<int> ans = majorityElement(arr);
+    // vector<int> ans = majorityElement(arr);
+    vector<int> ans2 = majorityElementBetterApproach(arr);
     cout << "The majority elements are: " << endl;
-    for(auto it : ans){
+    for(auto it : ans2){
         cout << it << " ";
     }
     cout << endl;
