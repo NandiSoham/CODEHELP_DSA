daff3c5d2fb12a66f46f004664de1336cdb6dfff
SohamNandiPDX <snandi@pdx.edu>
2024-02-27 23:34:31 -0800

    solved merge two sorted arrays uning GAP

--- 02_arrays/31_mergeSortedArrays.cpp
+++ 02_arrays/31_mergeSortedArrays.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 using namespace std;
 
+//-----------------------BRUTE FORCE -----------------------------------------
+
 void mergeArrayBruteForce(vector<int>& nums1, int n, vector<int>& nums2, int m) {
         int left = 0, right = 0, index = 0;
         vector<int> nums3(n+m, 0);
@@ -31,13 +33,61 @@ void mergeArrayBruteForce(vector<int>& nums1, int n, vector<int>& nums2, int m) 
 
     }
 
+//----------------------------------------------------------------
+
+//-----------------------APPROACH 2 (GAP)-----------------------------------------
+
+void swapIfGreater(vector<int>& nums1, int ind1, vector<int>& nums2, int ind2) {
+    if (nums1[ind1] > nums2[ind2]) {
+        swap(nums1[ind1], nums2[ind2]);
+    }
+}
+
+void mergeUsingGap(vector<int>& nums1, int n, vector<int>& nums2, int m) {
+    // len of the imaginary single numsay:
+    int len = n + m;
+
+    // Initial gap:
+    int gap = (len / 2) + (len % 2);
+
+    while (gap > 0) {
+        // Place 2 pointers:
+        int left = 0;
+        int right = left + gap;
+        while (right < len) {
+            // case 1: left in nums1[]
+            //and right in nums2[]:
+            if (left < n && right >= n) {
+                swapIfGreater(nums1, left, nums2, right - n);
+            }
+            // case 2: both pointers in nums2[]:
+            else if (left >= n) {
+                swapIfGreater(nums2, left - n, nums2, right - n);
+            }
+            // case 3: both pointers in nums1[]:
+            else {
+                swapIfGreater(nums1, left, nums1, right);
+            }
+            left++, right++;
+        }
+        // break if iteration gap=1 is completed:
+        if (gap == 1) break;
+
+        // Otherwise, calculate new gap:
+        gap = (gap / 2) + (gap % 2);
+    }
+}
+
+//----------------------------------------------------------------
+
 int main()
 {
     vector<int> nums1{1, 4, 8, 10};
     vector<int> nums2{2, 3, 9};
     int n = 4, m = 3;
     
-    mergeArrayBruteForce(nums1, n, nums2, m);
+    // mergeArrayBruteForce(nums1, n, nums2, m);
+    mergeUsingGap(nums1, n, nums2, m);
     cout << "The merged arrays are: " << "\n";
     cout << "nums1[] = ";
     for (int i = 0; i < n; i++) {
