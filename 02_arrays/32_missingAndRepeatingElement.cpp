1bd75f4e5e9a613cf60bbed20a23218f1786f33f
SohamNandiPDX <snandi@pdx.edu>
2024-02-28 15:11:43 -0800

    solved repeating and missing elements problem in brute and better approach both

---
+++ 02_arrays/32_missingAndRepeatingElement.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//-------------------------BRUTE FORCE---------------------------------------
+
+vector<int> findMissingAndRepeatingElementBrute(vector < int > arr) {
+    int n = arr.size();
+    int missingElemnt = -1, repeatingElement = -1;
+    for(int i = 1; i <= n; i++){
+        int count = 0;
+        for(int j = 0; j < n; j++){
+            if(arr[j] == i) count++;
+        }
+
+        if(count == 2) repeatingElement = i;
+        if(count == 0) missingElemnt = i;
+
+        if(repeatingElement != -1 && missingElemnt != -1) break;
+
+    }
+    return{repeatingElement, missingElemnt};
+}
+
+//----------------------------------------------------------------
+
+//--------------------------BETTER APPROACH(Using Hashing)--------------------------------------
+
+vector<int> findMissingRepeatingNumbersBetter(vector < int > arr) {
+    int n = arr.size();
+    int missingElemnt = -1, repeatingElement = -1;
+    int hash[n+1] = {0};
+    for(int i = 0; i < n; i++){
+        hash[arr[i]]++;
+    }
+
+    for(int i = 1; i <= n; i++){
+        if(hash[i] == 2) repeatingElement = i;
+        else if(hash[i] == 0) missingElemnt = i;
+
+        if(missingElemnt != -1 && repeatingElement != -1) break;
+    }
+    return{repeatingElement, missingElemnt};
+}
+
+int main()
+{
+    vector<int> arr = {3, 8, 2, 2, 4, 6, 7, 5};
+    // vector<int> ans = findMissingAndRepeatingElementBrute(arr);
+    vector<int> ans2 = findMissingAndRepeatingElementBrute(arr);
+    cout << "The repeating and missing elements are: " << ans2[0] << ", " << ans2[1] << endl;
+    return 0;
+}
