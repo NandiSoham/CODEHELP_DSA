28c3464801c34b6aba9a8206e2740e8d2b1b506d
SohamNandiPDX <snandi@pdx.edu>
2024-03-11 22:46:14 -0700

    solved binary search in iterative way

---
+++ 03_binarySearch/01_binarySearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//----------------------------Iterative Way------------------------------------
+
+int search(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+//----------------------------------------------------------------
+
+int main()
+{
+    vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
+    int target = 6;
+    int ind = search(a, target);
+    if (ind == -1)
+        cout << "The target is not present." << endl;
+    else
+        cout << "The target is at index: " << ind << endl;
+    return 0;
+}
