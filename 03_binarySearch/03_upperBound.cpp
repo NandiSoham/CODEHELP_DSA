f5a03e9e887f3f1746cf9cad7386d643d5ca7d2f
SohamNandiPDX <snandi@pdx.edu>
2024-03-12 18:23:59 -0700

    finding upper bound in optimal approach

--- 03_binarySearch/03_upperBound.cpp
+++ 03_binarySearch/03_upperBound.cpp
@@ -13,12 +13,35 @@ int upperBoundBrute(vector<int> &arr, int n, int target){
 
 //-------------------------------------------------------------------------------------------
 
+//---------------------------OPTIMAL APPROACH (Binary Search)--------------------------------
+
+int upperBoundOptimal(vector<int> &arr, int x, int n){
+	
+	int low = 0, high = n - 1;
+	int ans = n;
+	while(low<= high){
+		int mid = low + (high - low)/2;
+
+		if(arr[mid] > x){
+			ans = mid;
+			high = mid -1;
+		} else{
+			low = mid + 1;
+		}
+	}
+	return ans;	
+}
+
+//------------------------------------------------------------------------------------------
+
 int main()
 {
     vector<int> arr = {3, 5, 8, 9, 15, 19};
     int n = 5, target = 9;
 
     int upperBoundIndexBrute = upperBoundBrute(arr, n, target);
-    cout << "The upper bound index is: " << upperBoundIndexBrute   << endl;
+    int upperBoundIndexOptimal = upperBoundOptimal(arr, n, target);
+
+    cout << "The upper bound index is: " << upperBoundIndexOptimal   << endl;
     return 0;
 }
