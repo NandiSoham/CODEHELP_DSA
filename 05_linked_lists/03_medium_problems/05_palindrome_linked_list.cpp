497e7e7fe7b9d279624d24135d244d284078e684
SohamNandiPDX <snandi@pdx.edu>
2024-06-10 21:58:37 -0700

    solved palindrome linked list approach 1

---
+++ 05_linked_lists/03_medium_problems/05_palindrome_linked_list.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+// ------------------------------ Approach -1 ----------------------------
+
+bool isPalindromeApproach1(Node *head)
+{
+    Node *currNode = head;
+    vector<int> nodeArr;
+
+    while (currNode)
+    {
+        nodeArr.push_back(currNode->data);
+        currNode = currNode->next;
+    }
+
+    int i = 0, j = nodeArr.size() - 1;
+
+    while (i < j)
+    {
+        if (nodeArr[i] != nodeArr[j])
+            return false;
+        i++, j--;
+    }
+    return true;
+}
+
+// -----------------------------------------------------------------------
+
+void printLinkedList(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(1);
+    head->next = new Node(5);
+    head->next->next = new Node(2);
+    head->next->next->next = new Node(5);
+    head->next->next->next->next = new Node(1);
+
+
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    if (isPalindromeApproach1(head))
+    {
+        cout << "The linked list is a palindrome." << endl;
+    }
+    else
+    {
+        cout << "The linked list is not a palindrome." << endl;
+    }
+
+    return 0;
+}
