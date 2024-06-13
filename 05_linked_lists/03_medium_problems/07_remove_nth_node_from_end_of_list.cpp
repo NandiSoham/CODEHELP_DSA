b3436cad5e7f01ea2a4162c8ade43dd817354633
SohamNandiPDX <snandi@pdx.edu>
2024-06-12 23:13:27 -0700

    Solved Remove Nth Node From End of List (Approach-2)

--- 05_linked_lists/03_medium_problems/07_remove_nth_node_from_end_of_list.cpp
+++ 05_linked_lists/03_medium_problems/07_remove_nth_node_from_end_of_list.cpp
@@ -21,7 +21,6 @@ public:
     }
 };
 
-
 void printLL(Node *head)
 {
     while (head != NULL)
@@ -70,23 +69,54 @@ Node *removeNthFromEndApproach1(Node *head, int n)
 
 // -----------------------------------------------------------------------
 
+// ------------------------------ Approach -2 ----------------------------
+
+Node *removeNthFromEndApproach2(Node *head, int n)
+{
+    Node *temp = head;
+    Node *prev = head;
+
+    for (int i = 0; i < n; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return head->next;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+        prev = prev->next;
+    }
+
+    Node *delNode = prev->next;
+    prev->next = prev->next->next;
+    delete delNode;
+
+    return head;
+}
+
+// -----------------------------------------------------------------------
+
 int main()
 {
     // vector<int> arr = {1, 2, 3, 4, 5};
-    int N = 3;
-    Node *head = new Node(1);
-    head->next = new Node(2);
-    head->next->next = new Node(3);
-    head->next->next->next = new Node(7);
-    head->next->next->next->next = new Node(5);
+    int N = 5;
+    Node *head = new Node(4);
+    head->next = new Node(23);
+    head->next->next = new Node(37);
+    head->next->next->next = new Node(17);
+    head->next->next->next->next = new Node(55);
     head->next->next->next->next->next = new Node(6);
 
     cout << "Original Linked List: ";
     printLL(head);
     cout << endl;
 
-    head = removeNthFromEndApproach1(head, N);
-    
+    // head = removeNthFromEndApproach1(head, N);
+    head = removeNthFromEndApproach2(head, N);
+
     cout << "After Deleting The Linked List: ";
     printLL(head);
 
